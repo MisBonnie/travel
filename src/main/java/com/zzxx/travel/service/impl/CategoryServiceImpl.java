ed8175d4ca28111a606875b9d482de4cb2554948
Bonnie <hz_liuzb@163.com>
2021-03-19 09:40:16 +0800

    使用redis操作导航数据

--- src/main/java/com/zzxx/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/zzxx/travel/service/impl/CategoryServiceImpl.java
@@ -18,31 +18,31 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryDao categoryDao;
     @Autowired
-    private ShardedJedisPool shardedJedisPool;
+    private ShardedJedisPool jedisPool;
     @Override
     public List<Category> findAll() {
-        ShardedJedis jedis = shardedJedisPool.getResource();
-        // 1.从redis缓存中查询数据
-        Set<Tuple> categorys = jedis.zrangeWithScores("category", 0, -1);
-        if (categorys == null || categorys.size() == 0) {
-            System.out.println("从数据库查询....");
-            // 1.2查不到数据, 从数据库中查询并返回
+        ShardedJedis jedis = jedisPool.getResource();
+
+        Set<Tuple> category = jedis.zrangeWithScores("category", 0, -1);
+        if (category == null || category.size() == 0) {
+            System.out.println("从数据库查询...");
+            // 1.第一次从数据库中查询数据
             List<Category> list = categoryDao.findAll();
-            // 2.第一次从数据库中查询出来的数据,存储到redis缓存中
-            for(Category cate:list) {
-                jedis.zadd("category", cate.getCid(), cate.getCname());
+            // 1 门票 2 酒店 3 香港车票 ...
+            // 2.将查询出来的数据保存到redis数据库中
+            for (Category c : list) {
+                jedis.zadd("category", c.getCid(), c.getCname());
             }
-            jedis.close();
             return list;
         } else {
-            System.out.println("从redis查询....");
-            // 1.1查到数据, 直接将数据封装返回
+            System.out.println("从redis中查询...");
+            // 3.以后查询直接从redis中查询数据
             List<Category> categories = new ArrayList<>();
-            for (Tuple tuple : categorys) {
-                Category category = new Category();
-                category.setCid((int) tuple.getScore());
-                category.setCname(tuple.getElement());
-                categories.add(category);
+            for (Tuple tuple : category) {
+                Category cate = new Category();
+                cate.setCid((int) tuple.getScore());
+                cate.setCname(tuple.getElement());
+                categories.add(cate);
             }
             return categories;
         }
